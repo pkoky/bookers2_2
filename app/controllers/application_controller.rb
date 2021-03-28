38e4b1443f5c8daf473c3e3d0a50cb49947c07ac
pkoky <sscsk2224@gmail.com>
2021-03-28 10:36:47 +0000

    [Add]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     # devise利用の機能（ユーザ登録、ログイン認証など）が使われる場合、その前にconfigure_permitted_parametersが実行されます。
 
+    def after_sign_in_path_for(resource)
+      user_path(current_user)
+    end
+
+
 
     protected
 
